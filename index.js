e1d98719cd424135455239fe2bfbac6752e937c0
Richell Bonfield <richell.bonfield12@gmail.com>
2020-03-08 04:32:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const createFareMultiplier = function (multiplyValue) {
   };
 };
 
-const fareDoubler = createFareMultiplier(2);
+const fareDoubler = createFareMultiplier();
 
 const fareTripler = createFareMultiplier(3);
 
